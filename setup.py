3cfecc9548b9035f2fb13dff65563d14e4f19c68
kamikat <kamikat@banana.moe>
2017-06-16 14:38:15 +0800

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="feedo",
-      version="0.1dev",
+      version="1.0.0",
       description="Read, format and output an RSS stream.",
+      url='https://github.com/kamikat/feedo',
       author="Kamikat",
       author_email="kamikat@banana.moe",
+      install_requires=['feedparser'],
       packages=["feedo"],
       entry_points={
           'console_scripts': [ 'feedo = feedo:main' ] },
